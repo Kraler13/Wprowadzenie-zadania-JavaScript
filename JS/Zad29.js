c571b6be65ad14ec36371c1ad87f3a68a7b3fa44
fnowak <filip.nowak7@gmail.com>
2024-06-18 19:58:16 +0200

    Zad29

---
+++ JS/Zad29.js
@@ -0,0 +1,11 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 2;
+secondNumber = 4;
+
+function sum(num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(sum(firstNumber, secondNumber))
